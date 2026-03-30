71b703a37f3d0222c1855a028bf7136ac01c4882
Muntasir Albashir <Muntasir.Albashir@gmail.com>
2026-03-29 23:50:40 -0500

    fix iam permission on for eks nodes.

--- infra/iam.tf
+++ infra/iam.tf
@@ -17,8 +17,8 @@ resource "aws_iam_role" "cluster" {
   })
 }
 
-resource "aws_iam_role" "worker" {
-  name = "eks-worker-role"
+resource "aws_iam_role" "node" {
+  name = "eks-node-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -38,18 +38,18 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
 }
 
 
-resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
+resource "aws_iam_role_policy_attachment" "eks_node_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.worker.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.worker.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_container_registry" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.worker.name
+  role       = aws_iam_role.node.name
 }
 
