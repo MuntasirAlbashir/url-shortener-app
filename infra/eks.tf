70aec4c0f6f6ac656ceae5fddf65e7c94dbe1887
Muntasir Albashir <Muntasir.Albashir@gmail.com>
2026-03-29 23:14:06 -0500

    eks setup

---
+++ infra/eks.tf
@@ -0,0 +1,47 @@
+resource "aws_eks_cluster" "eks" {
+  name = "eks-compute-server"
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.35"
+
+  kubernetes_network_config {
+    ip_family = "ipv6"
+  }
+
+  vpc_config {
+    subnet_ids = aws_subnet.private[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_policy,
+  ]
+}
+
+resource "aws_eks_node_group" "workers" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "workers"
+  node_role_arn   = aws_iam_role.cluster.arn
+  subnet_ids      = aws_subnet.private[*].id
+
+  instance_types = [var.node_instance_type]
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry,
+    aws_iam_role_policy_attachment.eks_worker_policy,
+  ]
+}
